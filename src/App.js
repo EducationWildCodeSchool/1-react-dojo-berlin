7ecc49164f8e28bd618994aa2a8f9987672e1705
EducationWildCodeSchool <65235935+EducationWildCodeSchool@users.noreply.github.com>
2021-10-27 09:08:16 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+const initialProductList = [
+  { id: 1, name: 'product 1', price: 50, quantity: 1 },
+  { id: 2, name: 'product 2', price: 75, quantity: 2 },
+  { id: 3, name: 'product 3', price: 20, quantity: 5 },
+];
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>My Order</h1>
+    </div>
+  );
+}
+
+export default App;
